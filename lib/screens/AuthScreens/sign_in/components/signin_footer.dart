14fda7ed7f390731808c93c8e65b64634fb487e9
konnofuente <95084135+konnofuente@users.noreply.github.com>
2024-01-27 10:41:08 +0100

    sign in design completed formated

---
+++ lib/screens/AuthScreens/sign_in/components/signin_footer.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../../../../Theme/theme_export.dart';
+import '../../../../utils/utils_export.dart';
+import '../../../../widget/widget_export.dart';
+import 'package:mboa_pharmacie/models/Role.dart';
+import 'package:mboa_pharmacie/resources/value_manager.dart';
+import 'package:mboa_pharmacie/screens/AuthScreens/verifie_email.dart';
+import 'package:mboa_pharmacie/services/localisationService/t_key.dart';
+import 'package:mboa_pharmacie/screens/AuthScreens/sign_up/signup_screen.dart';
+
+
+
+class SignInFooter extends StatelessWidget {
+  const SignInFooter({
+    Key? key,
+    required this.role,
+  }) : super(key: key);
+
+  final Role? role;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        WidgetButton.textButton(TKeys.f_pwd.translate(context),
+            AppTextTheme.caption, () {
+          NavigationScreen.replaceNavigate(
+              context, const ConfirmEmail());
+        }),
+        const SizedBox(
+          height: AppSize.s20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            WidgetButton.textButton(
+                TKeys.did_not_have_acct.translate(context),
+                AppTextTheme.body2,
+                () {}),
+            role == Role.ADMIN
+                ? WidgetButton.textButton(
+                    TKeys.sign_up.translate(context),
+                    AppTextTheme.link, () {
+                    NavigationScreen.navigate(
+                        context, const SignUp());
+                  })
+                : WidgetButton.textButton(
+                    TKeys.request_acct.translate(context),
+                    AppTextTheme.link, () {
+                    AlertBox.awesomeOkBox(
+                        context,
+                        "Requette de Compte ",
+                        "Contacter l'administrateur pour la creation de votre compte",
+                        () {});
+                  }),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/AuthScreens/sign_in/signin_screen.dart
+++ lib/screens/AuthScreens/sign_in/signin_screen.dart
@@ -21,7 +21,9 @@ import 'package:mboa_pharmacie/utils/is_loading_indicator.dart';
 import 'package:mboa_pharmacie/resources/local_storage_key.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mboa_pharmacie/screens/AuthScreens/widget/export.dart';
+import 'package:mboa_pharmacie/screens/AuthScreens/sign_in/components/signin_footer.dart';
 import 'package:mboa_pharmacie/screens/AuthScreens/sign_in/components/email_login_form.dart';
+
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -92,131 +94,90 @@ class _SignInState extends State<SignIn> with TickerProviderStateMixin {
       body: !Provider.of<AppStatus>(context).isConnected
           ? isLoadingIndicator.CircularLoading(AppStrings.checkConnection)
           : SingleChildScrollView(
-              child: Container(
-                padding: getSymmetricPadding(AppPadding.p0, AppPadding.p40),
-                height: MediaQuery.of(context).size.height,
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    AuthPageTitle(
-                      title: TKeys.sign_in.translate(context),
-                    ),
-                    const SizedBox(
-                      height: AppSize.s12,
-                    ),
-                    Column(children: [
-                      Container(
-                          // height: 20,
-                          child: Text(
-                        TKeys.welcome.translate(context),
-                        style: AppTextTheme.header,
-                      )),
-                      const SizedBox(
-                        height: AppSize.s18,
+              child: Center(
+                child: Container(
+                  padding: getSymmetricPadding(AppPadding.p0, AppPadding.p40),
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      AuthPageTitle(
+                        title: TKeys.sign_in.translate(context),
                       ),
-                      AuthUserInfoWidget(role: role),
-                    ]),
-                    const SizedBox(
-                      height: AppSize.s28,
-                    ),
-                    Column(
-                      children: [
-                        TabBar(
-                          controller: _tabController,
-                          tabs: [
-                            Tab(text: 'Email Login'),
-                            Tab(text: 'Phone Login'),
-                          ],
-                        ),
+                      const SizedBox(
+                        height: AppSize.s12,
+                      ),
+                      Column(children: [
                         Container(
-                          padding: getSymmetricPadding(
-                              AppPadding.p0, AppPadding.p40),
-                          height: 300,
-                          width: double.infinity,
-                          child: TabBarView(
+                            // height: 20,
+                            child: Text(
+                          TKeys.welcome.translate(context),
+                          style: AppTextTheme.header,
+                        )),
+                        const SizedBox(
+                          height: AppSize.s18,
+                        ),
+                        AuthUserInfoWidget(role: role),
+                      ]),
+                      const SizedBox(
+                        height: AppSize.s28,
+                      ),
+                      Column(
+                        children: [
+                          TabBar(
                             controller: _tabController,
-                            children: [
-                              LoginForm(
-                                formKey: _formKey,
-                                emailController: emailController,
-                                emailKeytype: emailKeytype,
-                                isPasswordVisible: isPasswordVisible,
-                                passwordController: passwordController,
-                                phoneNumberController: phoneNumberController,
-                                phoneNumberKeytype: phoneNumberKeytype,
-                                isEmailFieldVisible: true,
-                                 isPhoneNumberFieldVisible: false,
-                              ),
-                              LoginForm(
-                                formKey: _formKey,
-                                emailController: emailController,
-                                emailKeytype: emailKeytype,
-                                isPasswordVisible: isPasswordVisible,
-                                passwordController: passwordController,
-                                phoneNumberController: phoneNumberController,
-                                phoneNumberKeytype: phoneNumberKeytype,
-                                isEmailFieldVisible: false,
-                                 isPhoneNumberFieldVisible: true,
-                              )
-
-                              // _buildPhoneLogin(context),
+                            tabs: [
+                              Tab(text: 'Email Login'),
+                              Tab(text: 'Phone Login'),
                             ],
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 40),
-                    WidgetButton.largeButton(
-                        TKeys.sign_in.translate(context),
-                        AppTextTheme.buttonwhite,
-                        AppColors.primary,
-                        null, () async {
-                      loginVerification();
-                    }),
-                    Column(
-                      children: [
-                        WidgetButton.textButton(TKeys.f_pwd.translate(context),
-                            AppTextTheme.caption, () {
-                          NavigationScreen.replaceNavigate(
-                              context, const ConfirmEmail());
-                        }),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            WidgetButton.textButton(
-                                TKeys.did_not_have_acct.translate(context),
-                                AppTextTheme.body2,
-                                () {}),
-                            role == Role.ADMIN
-                                ? WidgetButton.textButton(
-                                    TKeys.sign_up.translate(context),
-                                    AppTextTheme.link, () {
-                                    NavigationScreen.navigate(
-                                        context, const SignUp());
-                                  })
-                                : WidgetButton.textButton(
-                                    TKeys.request_acct.translate(context),
-                                    AppTextTheme.link, () {
-                                    AlertBox.awesomeOkBox(
-                                        context,
-                                        "Requette de Compte ",
-                                        "Contacter l'administrateur pour la creation de votre compte",
-                                        () {});
-                                  }),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.25,
+                            width: double.infinity,
+                            child: TabBarView(
+                              controller: _tabController,
+                              children: [
+                                LoginForm(
+                                  formKey: _formKey,
+                                  emailController: emailController,
+                                  emailKeytype: emailKeytype,
+                                  isPasswordVisible: isPasswordVisible,
+                                  passwordController: passwordController,
+                                  phoneNumberController: phoneNumberController,
+                                  phoneNumberKeytype: phoneNumberKeytype,
+                                  isEmailFieldVisible: true,
+                                   isPhoneNumberFieldVisible: false,
+                                ),
+                                LoginForm(
+                                  formKey: _formKey,
+                                  emailController: emailController,
+                                  emailKeytype: emailKeytype,
+                                  isPasswordVisible: isPasswordVisible,
+                                  passwordController: passwordController,
+                                  phoneNumberController: phoneNumberController,
+                                  phoneNumberKeytype: phoneNumberKeytype,
+                                  isEmailFieldVisible: false,
+                                   isPhoneNumberFieldVisible: true,
+                                )
+                                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: AppSize.s20),
+                      WidgetButton.largeButton(
+                          TKeys.sign_in.translate(context),
+                          AppTextTheme.buttonwhite,
+                          AppColors.primary,
+                          null, () async {
+                        loginVerification();
+                      }),
+                      SignInFooter(role: role),
+                    ],
+                  ),
                 ),
               ),
             ),
